c8300e36eb61ffef98312db038394808cf36bca4
rajibrahman74e <rajiburrahman74@gmail.com>
2023-02-16 09:18:34 +0600

    [ADD]-set-calculator-digit-in-display

--- js/app.js
+++ js/app.js
@@ -1,28 +1,61 @@
 
-function getOtt () {
-    const ott = randomOtt();
-    const ottString = ott + '';
-    if (ottString.length === 4) {
-        return ott;
-    } else {
-        return getOtt();
-    }
+// Convert 4 digits in random numbers
+function getOtt() {
+  const ott = randomOtt();
+  const ottString = ott + "";
+  if (ottString.length === 4) {
+    return ott;
+  } else {
+    return getOtt();
+  }
 }
 
 
 
-
+// Get Random
 function randomOtt() {
   const random = Math.round(Math.random() * 10000);
   return random;
 }
 
 
-document.getElementById('generate-btns').addEventListener('click', function () {
-    const randomOtt = getOtt();
+
+// Add even listener in Generate button
+
+document.getElementById("generate-btn").addEventListener("click", function () {
+  const randomOtt = getOtt();
+
+  // Disply ott
+  const ottField = document.getElementById("ott-field");
+  ottField.value = randomOtt;
+});
 
 
-    // Disply ott
-    const ottField = document.getElementById('ott-field');
-    ottField.value = randomOtt;
-})
+
+
+
+// calculator body
+
+document
+  .getElementById("calculator")
+  .addEventListener("click", function (event) {
+    const claculatorElementValue = event.target.innerText;
+
+    const calculatorFieldValue = document.getElementById("calculator-field");
+    const previousCalculatorFieldValue = calculatorFieldValue.value;
+
+    if (isNaN(claculatorElementValue)) {
+      if (claculatorElementValue === "C") {
+        calculatorFieldValue.value = "";
+      } else if (claculatorElementValue === "<") {
+        const digits = previousCalculatorFieldValue.split("");
+        digits.pop();
+        const remainingDigits = digits.join("");
+        calculatorFieldValue.value = remainingDigits;
+      }
+    } else {
+      const currentCalculatorFieldValue =
+        previousCalculatorFieldValue + claculatorElementValue;
+      calculatorFieldValue.value = currentCalculatorFieldValue;
+    }
+  });
